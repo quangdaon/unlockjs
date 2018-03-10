dd7a225f99f917fe07fa172354ae789a9f518ac3
quangdaon <quangdaon@gmail.com>
2018-03-10 15:23:40 -0600

    Updated press key simulator

--- test/specs/testutils.spec.js
+++ test/specs/testutils.spec.js
@@ -29,18 +29,34 @@ describe('Test Utilities', () => {
 			expect(keypressCallback).to.have.been.calledWith(sinon.match({ keyCode: 97 }));
 		});
 
-		it('should accept meta keys', () => {
+		it('should accept meta key', () => {
 			press(97, ['meta']);
 			expect(keypressCallback).to.have.been.calledWith(sinon.match({ keyCode: 97, metaKey: true }));
+		});
 
+		it('should accept shift key', () => {
 			press(97, ['shift']);
 			expect(keypressCallback).to.have.been.calledWith(sinon.match({ keyCode: 97, shiftKey: true }));
+		});
 
+		it('should accept ctrl key', () => {
 			press(97, ['ctrl']);
 			expect(keypressCallback).to.have.been.calledWith(sinon.match({ keyCode: 97, ctrlKey: true }));
+		});
 
+		it('should accept alt key', () => {
 			press(97, ['alt']);
 			expect(keypressCallback).to.have.been.calledWith(sinon.match({ keyCode: 97, altKey: true }));
+		});
+
+		it('should multiple meta keys', () => {
+			press(97, ['alt', 'ctrl']);
+			expect(keypressCallback).to.have.been.calledWith(sinon.match({ keyCode: 97, altKey: true, ctrlKey: true }));
+		});
+
+		it('should match meta keys exactly', () => {
+			press(97, ['alt', 'ctrl']);
+			expect(keypressCallback).to.have.been.calledWith(sinon.match({ keyCode: 97, altKey: true, ctrlKey: true, shiftKey: false, metaKey: false }));
 		});
 
 		it('should allow delegation to a specific element', () => {
--- test/utils/helpers.js
+++ test/utils/helpers.js
@@ -1,25 +1,21 @@
 export function press(code, keys, element) {
-	const specialKeys = {
-		ctrl: keys && keys.indexOf('ctrl') > -1,
-		alt: keys && keys.indexOf('alt') > -1,
-		shift: keys && keys.indexOf('shift') > -1,
-		meta: keys && keys.indexOf('meta') > -1
-	};
-
 	const delegate = element || document;
 
-	const evt = new KeyboardEvent('keydown', {
+	let evtOptions = {
 		bubbles: true,
 		composed: true,
 		charCode: code,
 		keyCode: code,
 		which: code,
-		key: String.fromCharCode(code),
-		shiftKey: specialKeys.shift,
-		ctrlKey: specialKeys.ctrl,
-		altKey: specialKeys.alt,
-		metaKey: specialKeys.meta
-	});
+		key: String.fromCharCode(code)
+	};
 
+	if (keys) {
+		keys.forEach(k => {
+			evtOptions[k + 'Key'] = true;
+		});
+	}
+
+	const evt = new KeyboardEvent('keydown', evtOptions);
 	delegate.dispatchEvent(evt);
 }
