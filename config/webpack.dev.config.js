dc02f7b553256a6b479d0a7838a5af877b3f90f8
Quangdao Nguyen <quangdaon@gmail.com>
2021-08-21 19:44:13 -0500

    Fix webpack conf

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,9 +1,17 @@
 const config = require('./webpack.config')();
+const path = require('path');
 
 config.mode = 'development';
 config.entry = {
 	unlock: ['./src/unlock.js'],
-	demo: ['./demo/index.js']
+	demo: ['./demo/index.js'],
+};
+config.devServer = {
+	static: {
+		directory: path.join(__dirname, '../demo'),
+	},
+	compress: true,
+	port: 9000,
 };
 
 module.exports = config;
